ad455cca62b196a4ce1cd1631a6a3af5c1daacb3
Tauhid Zaman <tauhid@techvisionco.com>
2021-11-27 02:01:07 +0600

    First check in

---
+++ MyMovieDBApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace MyMovieDBApp
+{
+    public static class TheMovieDBAPIConstants
+    {
+        public static string Url { get; set; }
+        public static string Key { get; set; }
+    }
+}
---
+++ MyMovieDBApp/Controllers/MovieController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMovieDBLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyMovieDBApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<string> GetAsync()
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage();
+
+            requestMessage.Method = HttpMethod.Get;
+            UriBuilder uriBuilder = new UriBuilder();
+            uriBuilder.Scheme = "https";
+            uriBuilder.Host = TheMovieDBAPIConstants.Url;
+            uriBuilder.Path = "movie/47964";
+            uriBuilder.Query = "api_key=" + TheMovieDBAPIConstants.Key;
+
+            requestMessage.RequestUri = uriBuilder.Uri;
+
+            HttpClient client = new HttpClient();
+            CancellationToken cancellationToken = default;
+            HttpResponseMessage resp = await client.SendAsync(requestMessage, cancellationToken).ConfigureAwait(false);
+            string result = resp.Content.ReadAsStringAsync().Result;
+            return result;
+        }
+    }
+}
